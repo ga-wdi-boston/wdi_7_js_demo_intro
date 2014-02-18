9a589f25cc2d4fb052c238dd9a54e4523e70db72
David Fisher <tibbon@gmail.com>
2014-02-18 10:59:17 -0500

    Added madlibs and euler 1 exercises

---
+++ madlibs.js
@@ -0,0 +1,8 @@
+var noun, verb, adverb, final_sentence;
+
+noun = prompt("Give me a noun");
+verb = prompt("Give me a verb");
+adverb = prompt("Give me a adverb");
+
+final_sentence = "Something " + noun + " words " + verb + " more words " + adverb;
+console.log(final_sentence);
--- main.js
+++ main.js
@@ -59,6 +59,56 @@ if(holyNumber > 3) {
   console.log("Throw the holy hand grenade");
 }
 
+// This is awesome in Ruby, but we don't have it in JS
+// colors.each do |color|
+//   puts color
+// end
+
+// This is all sorts of bad
+for(i = 0; i < colors.length; i++) {
+  console.log(colors[i]);
+}
+
+
+// for(number_you_will_increment_with; comparison of when to stop;
+//     what_to_do_at_end_of_loop) {
+//   stuff you want to do in the loop, with access to number;
+// }
+
+// 1 - no var
+// 2 - incrementing with ++
+// 3 - checking the length every iteration
+
+// Better for loops
+// Use a single var at the top of the scope
+// Declare the value of your incrementor, and logical checker
+var i = 0,
+    length = colors.length;
+
+// Leave off the first and last terms
+for(;i < length;) {
+  console.log(color[i]); // Do stuff, use `i`, output the color
+  i = i + 1; // Increment manually at end of loop
+}
+
+// We also have while loops
+var input = "";
+while(input !== "stop") {
+  input = prompt("type stop to stop");
+}
+
+// I don't like using switch statements much
+
+var n = 2;
+switch(n) {
+  case 1: // Do something if its 1
+  case 2: // Do something if its 2
+  default: // Anything that falls through
+}
+
+
+
+
 
 
 
