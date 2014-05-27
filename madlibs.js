e1d1542afeec440fb8637b3cc155d2325a968038
David Fisher <tibbon@gmail.com>
2014-05-27 10:36:54 -0400

    Updated for class demo and lab

--- madlibs.js
+++ madlibs.js
@@ -1,8 +1,2 @@
-var noun, verb, adverb, final_sentence;
-
-noun = prompt("Give me a noun");
-verb = prompt("Give me a verb");
-adverb = prompt("Give me a adverb");
-
-final_sentence = "Something " + noun + " words " + verb + " more words " + adverb;
-console.log(final_sentence);
+// Write a MadLibs (or Cards Against Humanity) prompt,
+// that asks you for at least 4 inputs, and then outputs a funny story/phrase on the console
